9dec9b9c54728cab2d05009ab24b62b94b44e6ce
eduramme <eduramme@gmail.com>
2024-07-05 14:13:55 -0300

    Amount item

--- src/components/atoms/ShutterCurrencySymbol.tsx
+++ src/components/atoms/ShutterCurrencySymbol.tsx
@@ -1,3 +1,27 @@
-export const ShutterCurrencySymbol = () => {
-  return <p className="text-sm text-white font-bold font-dm">$SHU</p>;
+import cc from "classcat";
+
+interface SymbolProps {
+  size?: ShutterCurrencySymbolSize;
+  style?: ShutterCurrencySymbolStyle;
+}
+
+export enum ShutterCurrencySymbolStyle {
+  Primary = "text-primary font-dm",
+  Secondary = "text-secondary font-dm",
+}
+
+export enum ShutterCurrencySymbolSize {
+  Big = "text-4xl font-medium",
+  Small = "text-base font-normal",
+}
+
+export const ShutterCurrencySymbol = ({
+  size = ShutterCurrencySymbolSize.Small,
+  style = ShutterCurrencySymbolStyle.Primary,
+}: SymbolProps) => {
+  return (
+    <p className={cc(["text-sm text-white font-bold font-dm", size, style])}>
+      $SHU
+    </p>
+  );
 };
--- src/components/molecules/Amount.tsx
+++ src/components/molecules/Amount.tsx
@@ -1,21 +1,16 @@
 import cc from "classcat";
 import { NumberValue, ShutterCurrencySymbol } from "../atoms";
 
-export enum NumberDisplayStyle {
-  PrimaryNumber = "text-primary font-dm",
-  SecondaryNumber = "text-secondary font-dm",
-}
-
-export enum NumberDisplaySize {
-  Big = "text-4xl font-medium",
-  Small = "text-base font-normal",
+enum AmountDisplaySize {
+  big = "big",
+  small = "small",
 }
 
 interface AmountProps {}
 
 export const Amount = ({ ...props }: AmountProps) => {
   return (
-    <div className="flex items-end bg-blue-500 p-4">
+    <div className="flex items-end rounded-md">
       <NumberValue label={50000} />
       <ShutterCurrencySymbol />
     </div>
