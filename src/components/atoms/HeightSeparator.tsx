ebb160171887098a3130ba668de95673dc4d524a
FrancoAguzzi <frankind.eth@gmail.com>
2024-07-08 17:07:40 -0300

    fix: adjust header nav in alignment to figma artboards

--- src/components/atoms/HeightSeparator.tsx
+++ src/components/atoms/HeightSeparator.tsx
@@ -1,3 +1,3 @@
 export const HeightSeparator = () => {
-  return <div className="h-full w-0 border-l border-gray-400" />;
+  return <div className="h-5 w-0 border-l border-[#2E383E]" />;
 };
--- src/components/organisms/DappHeader.tsx
+++ src/components/organisms/DappHeader.tsx
@@ -1,18 +1,19 @@
 import Image from "next/image";
 import { HeightSeparator, HeaderLink, ConnectWallet } from "@/components/atoms";
+import Link from "next/link";
 
 export const DappHeader = () => {
   return (
-    <div className="w-screen flex items-stretch mx-auto p-4 max-w-[1040px] justify-between">
-      <div className="flex items-center gap-4">
-        <div className="">
+    <div className="w-screen flex items-stretch mx-auto p-6 max-w-[1040px] justify-between">
+      <div className="flex items-center gap-6">
+        <Link href="/">
           <Image
-            src="/favicon.ico"
+            src="/shutter-icon.svg"
             alt="Shutter Staking"
-            width={120}
+            width={103}
             height={20}
           />
-        </div>
+        </Link>
         <HeightSeparator />
         <HeaderLink href="" label="DAOs" />
         <HeaderLink href="" label="Docs" />
