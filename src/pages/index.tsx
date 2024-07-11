19718f9f523d09085fef4a35601a555041fa7ed6
eduramme <eduramme@gmail.com>
2024-07-11 14:59:08 -0300

    Revert "feat: expose storybook"
    This reverts commit f4a227e18432d1023282c55bad3d47a442908022.

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -10,7 +10,6 @@ import {
   NumberDisplayStyle,
   NumberValue,
   ShutterCurrencySymbol,
-  StakesTable,
   StakingTable,
   TrophyIcon,
   WalletIcon,
@@ -27,7 +26,47 @@ export default function Home() {
       ])}
     >
       <div className="w-full grow grid grid-cols-2 gap-4 h-full">
-        <StakesTable />
+        <CardTemplate className="w-full flex flex-col h-full">
+          <div className="grid grid-cols-2 p-6 w-full h-full gap-4 flex-grow">
+            <div className="w-full h-full gap-3 flex flex-col items-start justify-center">
+              <p className="text-base font-medium font-dm">Staked Balance</p>
+              <div className="flex items-end gap-1">
+                <NumberValue label={130000} />
+                <ShutterCurrencySymbol />
+              </div>
+              <IconicButton
+                icon={<ArrowUp />}
+                label={"STAKE"}
+                onClick={() => alert("Execute STAKE action")}
+              />
+            </div>
+            <div className="w-full h-full flex gap-3 flex-col items-start justify-center">
+              <p className="text-base font-medium font-dm">Total Unlocked</p>
+              <div className="flex items-end gap-1">
+                <NumberValue
+                  numberDisplayStyle={NumberDisplayStyle.SecondaryNumber}
+                  label={52352}
+                />
+                <ShutterCurrencySymbol />
+              </div>
+              <IconicButton
+                icon={<ArrowDown />}
+                label={"UNSTAKE"}
+                onClick={() => alert("Execute unstake action")}
+              />
+            </div>
+          </div>
+          <div className="flex-grow min-h-[500px]">
+            <StakingTable />
+          </div>
+          <IconicButton
+            className="rounded-t-none"
+            iconPosition={IconPosition.RIGHT}
+            icon={<ArrowRight />}
+            label={"see more"}
+            onClick={() => alert("Execute see more action")}
+          />
+        </CardTemplate>
 
         <div className="flex h-full flex-col w-full gap-4">
           <div className="grid grid-cols-2 w-full h-ull gap-4">
